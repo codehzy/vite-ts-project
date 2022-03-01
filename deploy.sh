93f568c9d38b2bb7c24a06063284917087262686
codehzy <1257664828@qq.com>
2022-03-01 16:06:26 +0800

    ci(ci): 🐎 ci

--- deploy.sh
+++ deploy.sh
@@ -12,15 +12,12 @@ cd dist
 # 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b master
-git add -A
-git commit -m 'deploy'
+
 
 # 如果你要部署在 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:codehzy/vite-ts-tailwind.git master:gh-pages
+git push -f git@github.com:codehzy/vite-ts-project.git master:gh-pages
 
 cd -
